c71e0b36e2aa0206ef4b454d5e7440846659c06c
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-01-24 16:44:35 +0100

    Stack Navigation done

---
+++ navigators/RootStack.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+//colors
+import { Colors } from "./../components/styles";
+const { darkLight, brand, primary, tertiary, secondary } = Colors;
+
+// React Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// screens
+import Login from "./../screens/Login";
+import Signup from "./../screens/Signup";
+import Welcome from "./../screens/Welcome";
+
+const Stack = createStackNavigator();
+
+const RootStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "transparent",
+          },
+          headerTintColor: tertiary,
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeftContainerStyle: {
+            paddingLeft: 20,
+          },
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen
+          options={{ headerTintColor: darkLight }}
+          name="Welcome"
+          component={Welcome}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootStack;
